259800aa4ea19791696cce17900ffd953ac6a732
jpeg25 <jairo25509@hotmail.com>
2021-11-29 20:31:09 -0500

    index.css + import to style index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom";
-// import './index.css';
+import "./index.css";
 
-const Greeting = () => {
-  return <h2>Hola</h2>;
+const BookList = () => {
+  return (
+    <section className="book-list">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 };
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      className="book-image"
+      src="https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg"
+      alt=""
+    ></img>
+  );
+};
+
+function Title() {
+  return <h2>i love you to the moon and back</h2>;
+}
+
+function Author() {
+  return <h3>Adela Martínez </h3>;
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
